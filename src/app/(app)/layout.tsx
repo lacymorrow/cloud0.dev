b807ee5c7d151a0dfaee9a18377654a8b6bbff69
Lacy Morrow <me@lacymorrow.com>
2025-05-15 03:24:24 -0400

    auto commit

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from "next";
 import type React from "react";
 
+import { BrickMarquee } from "@/components/blocks/brick-marquee";
 import MainLayout from "@/components/layouts/main-layout";
 import { RootLayout } from "@/components/layouts/root-layout";
 import { metadata as defaultMetadata } from "@/config/metadata";
-
 export const metadata: Metadata = {
   ...defaultMetadata,
   manifest: "/manifest.json", // Ensure this path is correct
@@ -14,6 +14,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <RootLayout>
       <MainLayout>{children}</MainLayout>
+      <BrickMarquee />
     </RootLayout>
   );
 }
